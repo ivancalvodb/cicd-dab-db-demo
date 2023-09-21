2f0675b4c31f8c1dbb66384a77ea6e1e225643c3
ivancalvodb <ivan.calvo@databricks.com>
2023-09-21 10:45:36 -0600

    code changes on unit testing

---
+++ src/tests/test_unity_catalog.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import pytest
+import time
+
+import databricks
+from databricks.sdk import WorkspaceClient
+
+@pytest.fixture
+def ws_conn():
+    # Return the workspace connection, uses DATABRICKS_HOST and DATABRICKS_TOKEN env variables.
+    return WorkspaceClient(host = os.environ['DATABRICKS_HOST'], token = os.environ['DATABRICKS_TOKEN'])
+
+def test_dbfs_paths(ws_conn):
+    # - DBFS
+    # ws_conn.dbfs.exists()
+    assert True
+
+def test_unity_catalog_objects(ws_conn):
+    # - Catalogs
+    # ws_conn.catalogs.list()
+    # ws_conn.catalogs.get(name=...)
+
+    # - Schemas
+    # ws_conn.schemas.get(full_name=...)
+    # ws_conn.schemas.list(catalog_name=...)
+
+    # - Tables
+    # ws_conn.tables.get(full_name=table_full_name)
+    # ws_conn.tables.list(catalog_name=..., schema_name=...)
+    assert True
