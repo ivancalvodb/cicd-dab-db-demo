2bbe3771f5980db078b5dd770f84f4196ea783ed
ivancalvodb <ivan.calvo@databricks.com>
2023-10-17 12:06:14 -0600

    changes on file path, new one has been created

--- src/dlt-pipelines/notebook-etl.sql
+++ src/dlt-pipelines/notebook-etl.sql
@@ -6,7 +6,7 @@ CREATE INCREMENTAL LIVE TABLE turbine (
   CONSTRAINT correct_schema EXPECT (_rescued_data IS NULL)
 )
 COMMENT "Tuuurbine details, with location, wind turbine model type etc"
-AS SELECT * FROM cloud_files("/demos/manufacturing/iot_turbine/turbine", "json", map("cloudFiles.inferColumnTypes" , "true"))
+AS SELECT * FROM cloud_files("/Workspace/ts_demos/iot_data/turbine", "parquet", map("cloudFiles.inferColumnTypes" , "true"))
 
 -- COMMAND ----------
 
@@ -16,7 +16,7 @@ CREATE INCREMENTAL LIVE TABLE sensor_bronze (
   CONSTRAINT correct_energy EXPECT (energy IS NOT NULL and energy > 0) ON VIOLATION DROP ROW
 )
 COMMENT "Raw sensor data coming from json files ingested in incremental with Auto Loader: vibration, energy produced etc. 1 point every X sec per sensor."
-AS SELECT * FROM cloud_files("/demos/manufacturing/iot_turbine/incoming_data", "json", map("cloudFiles.inferColumnTypes" , "true"))
+AS SELECT * FROM cloud_files("/Workspace/ts_demos/iot_data/incoming_data", "parquet", map("cloudFiles.inferColumnTypes" , "true"))
 
 -- COMMAND ----------
 
@@ -25,7 +25,7 @@ CREATE INCREMENTAL LIVE TABLE historical_turbine_status (
   CONSTRAINT correct_schema EXPECT (_rescued_data IS NULL)
 )
 COMMENT "Turbine status to be used as label in our predictive maintenance model (to know which turbine is potentially faulty)"
-AS SELECT * FROM cloud_files("/demos/manufacturing/iot_turbine/historical_turbine_status", "json", map("cloudFiles.inferColumnTypes" , "true"))
+AS SELECT * FROM cloud_files("/Workspace/ts_demos/iot_data/historical_turbine_status", "parquet", map("cloudFiles.inferColumnTypes" , "true"))
 
 -- COMMAND ----------
 
